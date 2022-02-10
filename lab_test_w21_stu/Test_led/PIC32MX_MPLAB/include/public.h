36b489d339694598d4e680659b6bca928994c1e0
1934089 <1934089@D32206-26SPQ53.VanierCollege.intra>
2022-02-10 12:25:32 -0500

    LAB 2: Completed TaskB and TaskC

--- lab_test_w21_stu/Test_led/PIC32MX_MPLAB/include/public.h
+++ lab_test_w21_stu/Test_led/PIC32MX_MPLAB/include/public.h
@@ -13,7 +13,21 @@
 
 //#define     CONSOLE     C_LCD
 #define     CONSOLE     C_UART1
-    
+
+static void vTaskA(void *pvParameters);
 void vStartTask1(void);
 void vStartTask2(void);
+void TaskB();
+void vStartTaskB();
+void vInitQueue1(void);
+void vSendQueue1(int data);
+static void vTaskC( void *pvParameters );
+void vStartTaskC(void);
+void setSpeed(int _speed);
+int getSpeed(void);
+void vStartTaskC(void);
+void vInitTaskC(void);
+
 #endif /* PUBLIC_H */
+
+
